8e9ac5bea7671dd3f77feed97ad0cf3671a8a1f6
eliote-geeks <pauleliote97@gmail.com>
2024-07-31 15:04:29 +0200

    liaison classe niveau et specialité

--- app/Models/Niveau.php
+++ app/Models/Niveau.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Specialite;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Niveau extends Model
 {
     use HasFactory;
+    public function specialty()
+    {
+        return $this->belongsTo(Specialite::class);
+    }
 }
--- app/Models/Specialite.php
+++ app/Models/Specialite.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Niveau;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Specialite extends Model
 {
     use HasFactory;
+
+    public function niveau()
+    {
+        return $this->hasMany(Niveau::class);
+    }
 }
