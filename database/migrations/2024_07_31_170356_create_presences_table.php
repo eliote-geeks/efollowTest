6b4a83763e3c0011322e7433de69fe3017aa7779
eliote-geeks <pauleliote97@gmail.com>
2024-07-31 21:09:28 +0200

    update presence table

--- database/migrations/2024_07_31_170356_create_presences_table.php
+++ database/migrations/2024_07_31_170356_create_presences_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('presences', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->foreignId('student_id')->references('id')->on('students')->onDelete('cascade');
             $table->foreignId('program_id')->references('id')->on('programs')->onDelete('cascade');            
             $table->timestamps();
         });
