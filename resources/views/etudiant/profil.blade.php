f0654a66a861110f89b1c859dfdf87d3210baa9e
dylanalvin <dylanalvin28@gmail.com>
2024-08-01 12:03:25 +0100

    create profil file

---
+++ resources/views/etudiant/profil.blade.php
@@ -0,0 +1,83 @@
+<x-layouts>
+
+    <div class="content-body">
+            <div class="container-fluid">
+                
+                <div class="row">
+                    <div class="d-flex justify-content-between mb-3">
+                        <h4 class="card-titl" style="font-weight: bold; color: gray;">NOMBRE TOTAL DES ABSENCES DE NOM_DE_L'ETUDIANT: <strong>150 minutes</strong></h4>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <h4 class="mb-0 me-3" style="font-weight: bold; color: gray;">Historique des absences:</h4>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-striped verticle-middle table-responsive-sm" id="myTable">
+                                        <thead>
+                                            <tr class="text-dark">
+                                                <th scope="col">Jour</th>
+                                                <th scope="col">Cours</th>
+                                                <th scope="col">Heure de debut</th>
+                                                <th scope="col">Heure de fin</th>
+                                                <th scope="col">Nom du professeur</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Lundi, 02 Aout</td>
+                                                <td>maths</td>
+                                                <td>12:30</td>
+                                                <td>15:30</td>
+                                                <td>Professeur 1</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <h4 class="mb-0 me-3" style="font-weight: bold; color: gray;">Historique des présences:</h4>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-striped verticle-middle table-responsive-sm" id="myTable">
+                                        <thead>
+                                            <tr class="text-dark">
+                                                <th scope="col">Jour</th>
+                                                <th scope="col">Cours</th>
+                                                <th scope="col">Heure de debut</th>
+                                                <th scope="col">Heure de fin</th>
+                                                <th scope="col">Nom professeur</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Lundi, 02 Aout</td>
+                                                <td>maths</td>
+                                                <td>12:30</td>
+                                                <td>15:30</td>
+                                                <td>Professeur 1</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</x-layouts>
